a65a5ae4179584cb08e5b584294936f0b39f2730
Lolrit <36842092+Lolrit@users.noreply.github.com>
2020-10-02 16:44:37 +0530

    Update solver.R

--- R/solver.R
+++ R/solver.R
@@ -10,11 +10,11 @@ solver <- function(x,y){
   z4 <- x / y
   z5 <- y / x
   data.frame(
-    x + y = z
-    x - y = z1
-    y - x = z2
-    x * y = z3
-    x / y = z4
-    y / x = z5
+    cat("X + Y =",z)
+    cat("X - Y =",z1)
+    cat("Y - X =",z2)
+    cat("X * Y =",z3)
+    cat("X / Y =",z4)
+    cat("Y / X =",z5)
     )
 }
