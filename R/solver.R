82758d1440226cff856fed460282f45403fd5942
Lolrit <36842092+Lolrit@users.noreply.github.com>
2020-10-02 16:50:27 +0530

    Update solver.R

--- R/solver.R
+++ R/solver.R
@@ -10,9 +10,14 @@ solver <- function(x,y){
   z4 <- x / y
   z5 <- y / x
   cat("X + Y =",z)
+  print("")
   cat("X - Y =",z1)
+  print("")
   cat("Y - X =",z2)
+  print("")
   cat("X * Y =",z3)
+  print("")
   cat("X / Y =",z4)
+  print("")
   cat("Y / X =",z5)
 }
