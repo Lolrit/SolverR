eb23a12d2c16668a0da769c49e2b181dae5555f3
Lolrit <36842092+Lolrit@users.noreply.github.com>
2020-10-02 16:31:50 +0530

    Update solver.R

--- R/solver.R
+++ R/solver.R
@@ -4,15 +4,17 @@
 #' @param y second numeric variable
 solver <- function(x,y){
   z <- x + y
-  print("X + Y = "z)
   z1 <- x - y
-  print("X - Y = "z1)
   z2 <- y - x
-  print("Y - X = "z2)
   z3 <- x * y
-  print("X * Y = "z3)
   z4 <- x / y
-  print("X / Y = "z4)
   z5 <- y / x
-  print("Y / X = "z5)
+  data.frame(
+    x + y = z
+    x - y = z1
+    y - x = z2
+    x * y = z3
+    x / y = z4
+    y / x = z5
+    )
 }
